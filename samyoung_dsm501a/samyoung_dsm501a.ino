bded56433f468526c1112de3c5c4665b24625231
Vinothkumar Jayaraman <vinojv96@gmail.com>
2020-11-19 23:05:52 +0530

    code

---
+++ samyoung_dsm501a/samyoung_dsm501a.ino
@@ -0,0 +1,37 @@
+#include<string.h>
+byte buff[2];
+int pin = 9;//DSM501A input D8
+unsigned long duration;
+unsigned long starttime;
+unsigned long endtime;
+unsigned long sampletime_ms = 30000;
+unsigned long lowpulseoccupancy = 0;
+float ratio = 0;
+float concentration = 0;
+ 
+int i=0;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(9,INPUT);
+  starttime = millis(); 
+}
+void loop()
+{
+  duration = pulseIn(pin, LOW);
+  lowpulseoccupancy += duration;
+  endtime = millis();
+  if ((endtime-starttime) > sampletime_ms)
+  {
+    ratio = (lowpulseoccupancy-endtime+starttime + sampletime_ms)/(sampletime_ms*10.0);  // Integer percentage 0=>100
+    concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62; // using spec sheet curve
+    Serial.print("lowpulseoccupancy:");
+    Serial.print(lowpulseoccupancy);
+    Serial.print("    ratio:");
+    Serial.print(ratio);
+    Serial.print("    DSM501A:");
+    Serial.println(concentration);
+    lowpulseoccupancy = 0;
+    starttime = millis();
+  } 
+}
